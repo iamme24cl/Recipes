761b10a6c100a9032590c9b729755dac7c6f2728
Chandra Lama <iamme24cl@gmail.com>
2021-02-05 00:10:48 -0800

    implemented User signup/edit form using a form partial - new User able to sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,10 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		# raise params.inspect
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id 
 			redirect_to user_path(@user)
 		else
 			render :new
