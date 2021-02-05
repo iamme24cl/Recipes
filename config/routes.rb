0f6ac08ace0e74fd55c51fdb45755448cd49ccf1
Chandra Lama <iamme24cl@gmail.com>
2021-02-04 21:20:11 -0800

    added routes for signup, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#create'
+
   resources :reviews
   resources :ingredients
   resources :recipe_ingredients
   resources :recipes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
+  
 end
