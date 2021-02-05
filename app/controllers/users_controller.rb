80723d18819bcba1e68f2993f21d0135ccdf1ad5
Chandra Lama <iamme24cl@gmail.com>
2021-02-04 22:13:40 -0800

    wrote out UsersController and created view files for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+	before_action :set_user
+	skip_before_action :verify_user, only: [:new, :create]
+
+	def index
+		@users = User.all
+	end
+
+	def show
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
+	end
+
+	def set_user
+		@user = User.find_by(id: params[:id])
+	end
+
 end
