2bb9f3744dfea5f90a2515c5b0de722c90c5bdd7
Chandra Lama <iamme24cl@gmail.com>
2021-02-04 21:53:33 -0800

    implemented a navigation bar for the application using a parital in layouts, added current_user helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+	before_action :verify_user
+	helper_method :current_user
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+	end
+
+	def logged_in?
+		!!current_user
+	end
+
+	def verify_user
+		if !logged_in?
+			redirect_to root_path
+		end
+	end
+
 end
