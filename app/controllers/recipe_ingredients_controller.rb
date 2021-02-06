97b113e5ee995ac06b5c84ea48f25f46c07fca98
Chandra Lama <iamme24cl@gmail.com>
2021-02-06 09:50:07 -0800

    implemented recipe ingredient create and edit

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -3,14 +3,15 @@ class RecipeIngredientsController < ApplicationController
 	def new
 		if @recipe = Recipe.find_by(id: params[:recipe_id])
 			@recipe_ingredient = @recipe.recipe_ingredients.build
-			@recipe_ingredient.build_ingredient
+			@recipe_ingredient.build_ingredient		
 		end
 	end
 		
 
 	def create
 		# raise params.inspect
-		@recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)
+		@recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)	
+		# binding.pry
 		if @recipe_ingredient.save
 			redirect_to recipe_path(@recipe_ingredient.recipe)
 		else
@@ -19,9 +20,16 @@ class RecipeIngredientsController < ApplicationController
 	end
 
 	def edit
+		@recipe_ingredient = RecipeIngredient.find_by(id: params[:id])
 	end
 
 	def update
+		@recipe_ingredient = RecipeIngredient.find_by(id: params[:id])
+		if @recipe_ingredient.update(recipe_ingredient_params)
+			redirect_to recipe_path(@recipe_ingredient.recipe)
+		else
+			render :edit
+		end
 	end
 
 	private
@@ -29,4 +37,8 @@ class RecipeIngredientsController < ApplicationController
 	def recipe_ingredient_params
 		params.require(:recipe_ingredient).permit(:quantity, :recipe_id, :ingredient_id, ingredient_attributes: [:name])
 	end
+
+	# def ingredient_params
+	# 	params.require(:ingredient).permit(:name)
+	# end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,11 +1,11 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-  accepts_nested_attributes_for :ingredient 
+  accepts_nested_attributes_for :ingredient, reject_if: proc { |attributes| attributes['name'].blank? }
 
   validates :quantity, presence: true
 
-  # def ingredient_attributes=(attributes)
-  # 	ingredient = Ingredient.find_or_create_by(attributes) if !attributes['name'].blank?
+  # def ingredient_attributes=(ingredient_attributes)
+  # 	ingredient = Ingredient.find_or_create_by(ingredient_attributes) if !ingredient_attributes['name'].blank?
   # end
 end
