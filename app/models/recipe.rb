7927d425c4f503331f4ce1006b008b17eb92008d
Chandra Lama <iamme24cl@gmail.com>
2021-02-08 12:01:29 -0800

    implemented displaying avg_rating in Recipes show page and added dependent destroy for reviews as well

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-	has_many :reviews
+	has_many :reviews, dependent: :destroy
 	has_many :users, through: :reviews
 	belongs_to :user
 	has_many :recipe_ingredients, dependent: :destroy
