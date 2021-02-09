b8679c3c0e0e8efde8487d4204bc9a9110d919c9
Chandra Lama <iamme24cl@gmail.com>
2021-02-09 13:47:09 -0800

    added a private validation method for recipes to make sure the title is only saved in title case

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,6 +9,8 @@ class Recipe < ApplicationRecord
 	validates :title, presence: true
 	validates :cook_time, presence: true
 	validates :instructions, presence: true
+	validate :is_title_case
+	before_validation :make_title_case
 
 
 
@@ -22,6 +24,18 @@ class Recipe < ApplicationRecord
 			stars << review.stars
 		end
 		avg_stars = stars.sum(0.0) / stars.size
+	end
+
+	private
+
+	def is_title_case
+		if title.split.any?{|w|w[0].upcase != w[0]}
+			errors.add(:title, "Title must be in title case")
+		end
+	end
+
+	def make_title_case
+		self.title = self.title.titlecase
 	end
 end
   
