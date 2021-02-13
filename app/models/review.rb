927d7e261b612c3f2a44b250bc5d1fc298e8cd91
Chandra Lama <iamme24cl@gmail.com>
2021-02-12 23:30:32 -0800

    made some layout adjustments

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,6 +7,20 @@ class Review < ApplicationRecord
   	greater_than_or_equal_to: 0, less_than: 6}
   validates :content, presence: true
   validates :recipe, uniqueness: {scope: :user, message: ": This recipe has already been reviewed by you"}
+  validate :is_title_case
+	before_validation :make_title_case
 
+
+  private
+
+	def is_title_case
+		if title.split.any?{|w|w[0].upcase != w[0]}
+			errors.add(:title, "Title must be in title case")
+		end
+	end
+
+	def make_title_case
+		self.title = self.title.titlecase
+	end
 
 end
